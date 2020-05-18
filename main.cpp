31d34b99ff786cedca113df5c579bdd47516ed0b
MakRaamWilliam <48126243+MakRaamWilliam@users.noreply.github.com>
2020-05-18 19:39:05 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <iomanip>
 #include <list>
 #include <unordered_map>
-#include<bits/stdc++.h> 
+#include<bits/stdc++.h>
 #include <fstream>
 #include <sstream>
 #include <vector>
@@ -62,13 +62,11 @@ Opcodes* operations = Opcodes::getInstance();
 int main()
 {
    /* fstream source_file {"source.txt", ios::in}; // object for the input file.
-
     // checking if the source file opened succesfully.
     if(!source_file) {
         cout << "failed to open the source code file";
         exit(EXIT_FAILURE);
     }
-
     // defining sting to take line by line from the source program.
     string line;
     /**
@@ -77,23 +75,24 @@ int main()
      * operator = vector.at(1).
      * operand = vector.at(2).
     */
-   /* vector<string> data;
+    vector<string> data;
     while(getline(source_file, line)) {
         stringstream data_line(line);
-
         for(string s; data_line >> s; )
+            if(data.empty() && (isderctive(s) || operations->getopcode(s)!= "null" )
+               data.push_back("");
             data.push_back(s);
-
-        if(data.size() > 3) 
-            if(data.at(3).at(0) != data.at(3).at(1)) 
+        if(data.size() > 3)
+            if(data.at(3).at(0) != data.at(3).at(1))
                 printErrorMessage();
-            
+        if(data.size()<3) data.push_back("");
         if(data.at(0).at(0) == '.') {
             data.clear();
             continue;
         }
+
         ++lineNO;
-    }*/
+    }
     cout << operations << endl;
     Opcodes* ass = Opcodes::getInstance();
     cout << ass << endl;
@@ -104,6 +103,34 @@ int main()
 bool label_validaty_checker(string str) {
     return (isdigit(str.at(0)) ? false : true);
 }
+
+bool valid_line(vector<string> data){
+  if( data[0] != "" && !label_validaty_checker(data[0])  )
+     return false;
+   if(isderctive(data[1])){
+    if(data[0] == "" || data[2]=="")
+        return false;
+   }
+     else{
+   string opc =operations->getopcode(data[1]);
+   if( opc =="null" ) return false;
+
+   if ( (data[2]=="" && opc != "4c") || (data[2]!="" && !label_validaty_checker(data[2]) )  )
+    return false;
+     }
+
+ return true;
+
+}
+
+bool isderctive(string s ){
+  if(s == "WORD" ||s== "BYTE"|| s=="RESW"||s=="RESB"  )
+    return true;
+  return false;
+
+}
+
+
 
 bool caseInSensStringCompareCpp11(string str1, string str2)
 {
