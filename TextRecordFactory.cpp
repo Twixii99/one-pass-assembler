92872d6d957083fd2c5c6ef290f779b3c68ac574
slfyzz <48367677+slfyzz@users.noreply.github.com>
2020-05-19 16:19:18 +0200

    Update TextRecordFactory.cpp

--- TextRecordFactory.cpp
+++ TextRecordFactory.cpp
@@ -33,7 +33,7 @@ Sym* TextRecordFactory::modifyPrevAddress(string label) {
     Symtable* table = Symtable::getInstance();
     Sym* symb = table->getSymbol(label);
 
-    if (symb == nullptr || symb->address == "*" || symb->operandsNeedThisLabel.size() == 0)
+    if (symb == nullptr || symb->address != "*" ||symb->operandsNeedThisLabel.size() == 0)
         return nullptr;
 
     return symb;
@@ -48,21 +48,29 @@ bool TextRecordFactory::addTextRecord(const vector<string>& statement, int locct
             cout << textRecord->tostring() << '\n';
         }
         status = textRecord->addText(statement, symb);
-        symb->operandsNeedThisLabel.clear();
         textRecord->newText(locctr);
     }
     else {
         // i dont know the limit :(
-        if (textRecord->tostring().size() > 30){
+        if (textRecord->tostring().size() / 2 + numberOfBytes(statement) > 30){
             cout << textRecord->tostring() << '\n';
             textRecord->newText(locctr);
         }
+        else if (textRecord->tostring().size() == 0)
+            textRecord.newText(locctr);
         status = textRecord->addText(statement);
     }
     return status > 0;
 }
 
 
-
+int numberOfBytes(const vector<string>& statement)
+{
+    if (statement[1][0] == '+')
+        return 4;
+    else if (statement[1][statement[1].size()-1] == 'r' || statement[1][statement[1].size()-1] == 'R')
+        return 2;
+    return 3;
+}
 
 
