7635c65e71003ec2ae8b173198577cb2293fd2a2
Twixii <lilkamal76@gmail.com>
2020-05-23 06:54:55 +0200

    parsing is finished 2

--- parsing .h
+++ parsing .h
@@ -7,13 +7,13 @@
 
 class parsing{
 
- int display(std::vector<std::string> &statement);
  void setnumofBytes( std::vector<std::string> &statement);
  void setaddressmode( std::vector<std::string> &statement);
  void checkParsing( std::vector<std::string> &statement);
  void parseDirective(std::vector<std::string> &statement);
  bool valid,pcrel ;
  public:
+   int display(std::vector<std::string> &statement);
    int numofBytes,locc; long long modesaddress;
    parsing();
    bool isvalid();
